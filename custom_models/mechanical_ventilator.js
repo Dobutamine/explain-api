edfa94677f50bad51e6872a131f9776ac89fe22c
dobutamine <tim.antonius@outlook.com>
2022-04-11 20:59:43 +0200

    update

--- custom_models/mechanical_ventilator.js
+++ custom_models/mechanical_ventilator.js
@@ -1,12 +1,21 @@
 class MechanicalVentilator {
   constructor(_model) {
+    // get a reference to the model
     this.model = _model;
+
+    // set as non initialized when constructed
     this.initialized = false;
   }
   init() {
     this.initialized = true;
   }
-  model_step() {}
+  model_step() {
+    if (this.is_enabled) {
+      this.ventilate();
+    }
+  }
+
+  ventilate() {}
 }
 
 module.exports = MechanicalVentilator;
